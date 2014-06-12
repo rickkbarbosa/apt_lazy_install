38f48884fba32e6dac0fed390b81fb954ce827a1
Rickk Barbosa <ri.blackangel@gmail.com>
2014-06-12 10:46:18 -0300

    Atualização de invocação dos arquivos extras

--- apt_lazy_install.sh
+++ apt_lazy_install.sh
@@ -7,20 +7,20 @@ DISTRO=$(lsb_release -c | awk '{print $2}';)
 . config/repos.inc
 
 #Begin
-echo "Updating APT-Cache"
+echo "Updatin. config/apt-Cache"
 apt-get update --allow-unauthenticated
 
 #Install Apps
 echo "Installing your applications"
-apt-get install -y ssh aptitude vim zsh
-. apt_vga_cards
-. apt_desktop_environments
-. apt_dev_tools
-. apt_internet_apps
-. apt_multimedia_apps
-. apt_support_tools
-. apt_tools
-. apt_dev_php_mysql_environments
+apt-get install -y ss. config/aptitude vim zsh
+. config/apt_vga_cards
+. config/apt_desktop_environments
+. config/apt_dev_tools
+. config/apt_internet_apps
+. config/apt_multimedia_apps
+. config/apt_support_tools
+. config/apt_tools
+. config/apt_dev_php_mysql_environments
 
 #Upgrade all system
 echo "Upgrading system"
@@ -28,4 +28,4 @@ apt-get upgrade -y
 
 #Remove boring things (At least form me They are :p )
 echo "Unable other packages"
-. apt_remove_unsused_packages
+. config/apt_remove_unsused_packages
